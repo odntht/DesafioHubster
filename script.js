0a63fc8612990b4e9b8fa9a9bf828828d13e7320
odntht <odntht@gmail.com>
2019-08-21 18:40:49 -0300

    UPDATE - Construindo divs com img e dados

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ var jqxhr = $.get( "http://5d556e6936ad770014cce06d.mockapi.io/api/v1/catalog/pr
 .fail(function() {
   alert( "Erro ao solicitar a API!" );
 });
-
 // Fim da Chamada AJAX
-
 
 // FUnção que constrói a lista de Categorias(Filtros)
 function constroiCategorias(categorias) {
@@ -29,16 +27,28 @@ function constroiCategorias(categorias) {
 
 // FUnção que constróí a lista de Produtos
 function constroiProdutos(produtos){
-  console.log(produtos);
+  // console.log(json);
   for (let i=0 ; i< produtos.length; i++){
-    if (i % 2 == 0){
-      console.log('par')
+    if (produtos[i].category.id == 15889) {
+      $("#produtos").append(`
+        <div class="card mb-3 col-6 ` + produtos[i].category.id + `" id="` + produtos[i].id + `" data-toggle="tooltip" data-placement="bottom" title="` + produtos[i].description + `">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img class="img-fluid rounded " src="` + produtos[i].photo + `" alt="Foto do ` + produtos[i].name + `">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h6 class="card-title">` + produtos[i].name + `</h5>
+                <p class="card-text"> R$ ` + produtos[i].salePrice + `</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        `);
+      }
     }
-  $("#produtos").append(`
-    <button id="` + produtos[i].id + `" type="button" class="btn btn-secondary ` + produtos[i].category.id + `" data-toggle="tooltip" data-placement="bottom" title="` + produtos[i].description + `">
-      <img src="` + produtos[i].photo + `" alt="Foto do ` + produtos[i].name + `" height="42" width="42">
-      <span>` + produtos[i].name + `</span>
-      <span> R$` + produtos[i].salePrice + `</span>
-    </button>`);
-}
-}
+  }
+
+  function mudaCategoria(produtos, categoria) {
+
+  }
